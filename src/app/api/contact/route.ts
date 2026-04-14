023d667d0f24396c8575cbf9c37dcd25f19611aa
PatrickHortreiter <patrick@newbeemountain.com>
2026-04-14 09:53:24 +0200

    fixing

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -40,7 +40,6 @@ export async function POST(req: NextRequest) {
         .filter(Boolean)
         .join("\n"),
     });
-
     return NextResponse.json({ success: true });
   } catch (err) {
     console.error("Mailgun error:", err);
