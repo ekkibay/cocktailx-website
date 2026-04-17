4167570974afb2c1bc180c6f3eecdc10459e7afa
PatrickHortreiter <patrick@newbeemountain.com>
2026-04-17 11:07:58 +0200

    main merge

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,7 +35,6 @@ export default async function LocaleLayout({
   if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
     notFound();
   }
-
   const messages = await getMessages();
 
   return (
