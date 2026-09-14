f26247f11769d2106c30153c24cfdc1cfc684014
PatrickHortreiter <patrick@newbeemountain.com>
2026-09-14 16:00:24 +0200

    trigger vercel

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -49,7 +49,6 @@ export default async function LocaleLayout({
   params: { locale: string };
 }) {
   const { locale } = params;
-
   if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
     notFound();
   }
